5abefe4a65281350112f5aa66ea85bbf6042086c
Lucas <49001666+lunascripts2@users.noreply.github.com>
2019-04-07 07:01:59 +0000

    Update owlmailer.php

--- owlmailer.php
+++ owlmailer.php
@@ -12,6 +12,7 @@ ini_set("memory_limit",-1);
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
+header("Access-Control-Allow-Origin: *");
 
  
 $password = ""; // Password 
